9bd70138aa280cd5886b004c19252668f67e2c5e
Zakharov Rustam <rustamz2000@gmail.com>
2024-10-24 13:14:09 +0300

    feet: create second page

---
+++ lib/second_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.blueAccent,
+        ),
+      ],
+    );
+  }
+}
